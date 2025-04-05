82a76759cb0e3fa5fbb0cf71664d90a475e5daad
곽동욱 <kdw381@naver.com>
2025-04-06 02:27:09 +0900

    first assignment OOP

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,93 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        BabyLionList helper = new BabyLionList(100); //최대인원 설정
+        int select;
+        while(true){
+            System.out.println("=======아기사자들=======");
+            System.out.println("1. 아기사자등록");
+            System.out.println("2. 아기사자 전체조회");
+            System.out.println("3. 아기사자 개별조회(학번)");
+            System.out.println("4. 아기사자 자기소개 수정");
+            System.out.println("5. 아기사자 성실도 변경");
+            System.out.println("6. 아기사자 삭제");
+            System.out.println("7. 종료");
+            System.out.println("메뉴선택 :");
+            select = scanner.nextInt();
+            scanner.nextLine(); //버퍼에 남은 엔터 제거
+
+            if(select == 1){
+                System.out.println("이름 :");
+                String name = scanner.nextLine();
+                System.out.println("학번: ");
+                int std_num = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("자기소개: ");
+                String introduce = scanner.nextLine();
+                System.out.println("성실도 (0~5): ");
+                int much_nice = scanner.nextInt();
+                scanner.nextLine();
+                helper.addBLion(new BabyLion(name,std_num,introduce,much_nice));
+                System.out.println("아기사자 추가완료!");
+            }
+            if(select == 2){
+                System.out.println("아기사자 전체조회");
+                helper.allBabyLion();
+
+            }
+            if(select == 3){
+                System.out.println("아기사자 개별조회");
+                System.out.println("학번 입력 :");
+                int a = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(helper.findStdNum(a));
+            }
+            if(select == 4){
+                System.out.println("아기사자 자기소개 수정하기");
+                System.out.println("학번 입력 :");
+                int b = scanner.nextInt();
+                scanner.nextLine();
+                String c = scanner.nextLine();
+                System.out.println("수정할 내용 : ");
+
+                System.out.println(helper.updateIntroduce(b,c));
+            }
+            if(select == 5){
+                System.out.println("학번 입력: ");
+                int d = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("1. 성실도 +1");
+                System.out.println("2. 성실도 -1");
+                System.out.print("선택: ");
+                int e = scanner.nextInt();
+                scanner.nextLine();
+
+                String result;
+                if (e == 1) {
+                    result = helper.increaseNice(d);
+                } else if (e == 2) {
+                    result = helper.decreaseNice(d);
+                } else {
+                    result = "잘못된 선택입니다.";
+                }
+
+                System.out.println(result);
+            }
+            if(select == 6){
+                System.out.println("학번 입력 : ");
+                int f = scanner.nextInt();
+                scanner.nextLine();
+                helper.removeBLion(f);
+
+            }
+            if(select == 7){
+                System.out.println("종료하겠습니다.");
+                break;
+            }
+
         }
+
     }
 }
